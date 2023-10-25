5bb86f482f09d98ccbc28c3f2791d28333151f2e
AldoMekic <aldomekic@gmail.com>
2023-10-25 22:47:08 +0200

    Added a new class that uses the former class

--- shopProduct.php
+++ shopProduct.php
@@ -7,17 +7,17 @@
 </head>
 <body>
     <?php
-        # Created the class ShopProduct
+        
         class ShopProduct
         {
-            # Created its arguments
+            
             public $title;
             public $producerMainName;
             public $producerFirstName;
             public $price = 0;
 
-            # Added a constructor for the objects
-            public function __construct($title="", $firstName="", $mainName="", $price=0)
+           
+            public function __construct(string $title,string $firstName,string $mainName,float $price)
             {
                 $this->title = $title;
                 $this->producerFirstName = $firstName;
@@ -25,38 +25,29 @@
                 $this->price = $price;
             }
 
-            # Added a get method for the producer's first and main name
+            
             public function getProducer()
             {
-                return "$this->producerFirstName $this->producerMainName <br>";
+                return "$this->producerFirstName $this->producerMainName ";
             }
         }
 
-        # Created two ShopProduct objects, whose values are created through the constructor
-        $product1 = new ShopProduct("","","",100);
-        $product2 = new ShopProduct("Another one", "John", "Cox", 150);
+        # Created another class, ShopProductWriter, that uses the ShopProduct class in its implementation
+        class ShopProductWriter
+        {
+            # A method that writes out all the values of a ShopProduct object
+            public function write(ShopProduct $shopProduct)
+            {
+                $str = $shopProduct->title . " : " . $shopProduct->getProducer() . "(" . $shopProduct->price . ") <br>";
+                return $str;
+            }
+        }
 
-        # Showing the types of the objects, which is ShopProduct
-        var_dump($product1);
-        echo "<br>";
-        var_dump($product2);
-        echo "<br>";
+        # Testing of the write method
+        $product1 = new ShopProduct("Rip and Tear","Aldin","Mekic",100.5);
+        $writer = new ShopProductWriter();
+        echo $writer->write($product1);
 
-        # Testing how calling specific arguments of an object can work in the program, first the call of the default value and
-        # then a change to the called argument and the call for its changed state
-        echo "$product1->title <br>";
-        $product1->title = "Rip and Tear";
-        echo "$product1->title <br>";
-
-        # We can also create new arguments outside the class itself, like this
-        $product2->addedArgument = 10;
-        echo "$product2->addedArgument <br>";
-
-        # Testing the getProducer() method. With the constructor initiated at the start, we can simply call $product2
-        $product1->producerFirstName = "Aldin1";
-        $product1->producerMainName = "Mekic1";
-        echo $product1->getProducer();
-        echo $product2->getProducer();
     ?>
 </body>
 </html>
