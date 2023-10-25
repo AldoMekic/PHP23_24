c63754f3056dff7d6374b5448c6c1f77513050b5
AldoMekic <aldomekic@gmail.com>
2023-10-25 22:42:57 +0200

    Showing the types of data of the objects

--- shopProduct.php
+++ shopProduct.php
@@ -9,8 +9,15 @@
     <?php
         class ShopProduct
         {
-            
+
         }
+
+        $product1 = new ShopProduct();
+        $product2 = new ShopProduct();
+
+        var_dump($product1);
+        echo "<br>";
+        var_dump($product2);
     ?>
 </body>
 </html>
