82c2403fa498285274b3b59dd720d884ca0ac4db
AldoMekic <aldomekic@gmail.com>
2023-10-25 22:44:36 +0200

    Additional changes and added comments

--- shopProduct.php
+++ shopProduct.php
@@ -7,17 +7,35 @@
 </head>
 <body>
     <?php
+        # Created the class ShopProduct
         class ShopProduct
         {
-
+            # Created its arguments
+            public $title = "default product";
+            public $producerMainName = "main name";
+            public $producerFirstName = "first name";
+            public $price = 0;
         }
 
+        # Created two ShopProduct objects
         $product1 = new ShopProduct();
         $product2 = new ShopProduct();
 
+        # Showing the types of the objects, which is ShopProduct
         var_dump($product1);
         echo "<br>";
         var_dump($product2);
+        echo "<br>";
+
+        # Testing how calling specific arguments of an object can work in the program, first the call of the default value and
+        # then a change to the called argument and the call for its changed state
+        echo "$product1->title <br>";
+        $product1->title = "Rip and Tear";
+        echo "$product1->title <br>";
+
+        # We can also create new arguments outside the class itself, like this
+        $product2->addedArgument = 10;
+        echo "$product2->addedArgument <br>";
     ?>
 </body>
 </html>
